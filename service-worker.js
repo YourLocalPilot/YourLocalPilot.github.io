fe4ce163a279050cc24a56da9da6a0cc1c12016b
dAS Barcode Scanner <115914048+YourLocalPilot@users.noreply.github.com>
2024-07-30 14:39:45 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const cacheName = 'bag-tag-scanner-cache-v1';
+const assetsToCache = [
+  '/',
+  '/bag.html',
+  '/styles.css',
+  '/script.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then((cache) => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
